43311ab792cbe332f277e71b7d33b548119474b0
fescudie <escudie.frederic@iuct-oncopole.fr>
2020-05-07 17:17:32 +0200

    Add script to automate assessment.

---
+++ assessment/bin/launch_eval.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+set -e # exit when any command fails
+
+# Parameters and environment
+if [ $# -eq 0 ]
+then
+    echo -e "[\033[0;31mERROR\033[0m] The workflow version is required"
+    exit 1
+fi
+wf_version=$1
+
+assessment_bin_dir=`dirname $0`
+assessment_bin_dir=`realpath ${assessment_bin_dir}`
+assessment_dir=`dirname ${assessment_bin_dir}`
+export PATH=${assessment_bin_dir}:$PATH
+
+# Evaluate datasets
+declare -a datasets=('Heyer_2019', 'Tembe_2014')
+for dataset_name in "${datasets[@]}"
+do
+	echo "Process "${dataset_name}
+	evalVCFRes.py \
+	 -d ${dataset} \
+	 -m genes \
+	 -e datasets/litterature_expected_fusion_list.tsv \
+	 -a results/${wf_version}/${dataset}/vcf/*_unfiltered.vcf \
+	 -o results/${wf_version}/${dataset}/results_genes.tsv
+	evalVCFRes.py \
+	 -d ${dataset} \
+	 -m breakpoints \
+	 -e datasets/litterature_expected_fusion_list.tsv \
+	 -a results/${wf_version}/${dataset}/vcf/*_unfiltered.vcf \
+	 -o results/${wf_version}/${dataset}/results_breakpoints.tsv
+done
+
+# Concatenate all datasets
+grep "dataset\tsample_ID" results/${wf_version}/Heyer_2019/results_genes.tsv > results/${wf_version}/results_details_genes.tsv
+grep -v "dataset\tsample_ID" results/${wf_version}/*/results_genes.tsv >> results/${wf_version}/results_details_genes.tsv
+
+grep "dataset\tsample_ID" results/${wf_version}/Heyer_2019/results_breakpoints.tsv > results/${wf_version}/results_details_breakpoints.tsv
+grep -v "dataset\tsample_ID" results/${wf_version}/*/results_breakpoints.tsv >> results/${wf_version}/results_details_breakpoints.tsv
+
+# Process performance metrics
+evalVCFMetrics.py \
+ -i results/${wf_version}/results_details_genes.tsv \
+ -o results/${wf_version}/results_metrics_genes.tsv
+evalVCFMetrics.py \
+ -i results/${wf_version}/results_details_breakpoints.tsv \
+ -o results/${wf_version}/results_metrics_breakpoints.tsv
+
+echo "End of job"
