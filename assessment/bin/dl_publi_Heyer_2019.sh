13a1466d3ce52800f8ebc368db0e243233164a1d
fescudie <escudie.frederic@iuct-oncopole.fr>
2024-01-21 20:43:42 +0100

    Update test datasets.

--- assessment/bin/dl_publi_Heyer_2019.sh
+++ assessment/bin/dl_publi_Heyer_2019.sh
@@ -27,7 +27,7 @@ rename () {
 }
 
 # Clean constant sequence starting reads
-cut () {
+cutseq () {
 	mv $2/$1_R1.fastq.gz $2/$1_R1_tmp.fastq.gz && \
 	cutadapt -u 12 -o $2/$1_R1.fastq.gz $2/$1_R1_tmp.fastq.gz && \
 	rm $2/$1_R1_tmp.fastq.gz && \
@@ -54,68 +54,94 @@ then
 fi
 
 # Download
-declare samples=(
-	#~ 	"SRR7646937"  # Problem in file
-	#~ 	"SRR7646901"  # Fusion not verified by other method
-	#~ 	"SRR7646885"  # Fusion not verified by other method
-	#~ 	"SRR7646959"  # Problem in file
-	"SRR7646928"
-	"SRR7646929"
-	"SRR7646934"
-	"SRR7646935"
-	"SRR7646932"
-	"SRR7646933"
-	"SRR7646936"
-	"SRR7646890"
-	"SRR7646896"
-	"SRR7646902"
-	"SRR7646898"
-	"SRR7646906"
-	"SRR7646907"
-	"SRR7646952"
-	"SRR7646955"
-	"SRR7646948"
-	"SRR7646951"
+declare -a samples=(
 	"SRR7646882"
 	"SRR7646883"
+	# "SRR7646885"  # Fusion not verified by other method
+	"SRR7646890"
+	"SRR7646896"
+	"SRR7646898"
+	# "SRR7646901"  # Mate partner unknown 
+	"SRR7646902"
+	"SRR7646906"
+	"SRR7646907"
+	"SRR7646914"
+	"SRR7646915"
+	"SRR7646917"
+	"SRR7646919"
+	"SRR7646924"
+	"SRR7646926"
+	"SRR7646927"
+	"SRR7646928"
+	"SRR7646929"
+	"SRR7646930"
+	"SRR7646931"
+	"SRR7646932"
+	"SRR7646933"
+	"SRR7646934"
+	"SRR7646935"
+	"SRR7646936"
+	# "SRR7646937"  # Problem in file: Reads are improperly paired
 	"SRR7646942"
+	"SRR7646948"
+	"SRR7646951"
+	"SRR7646952"
+	"SRR7646955"
 	"SRR7646961"
 	"SRR7646962"
 	"SRR7646963"
 	"SRR7646965"
 	"SRR7646966"
 	"SRR7646967"
-	"SRR7646919"
+	"SRR7646879"
+	"SRR7646895"
+	"SRR7646899"
+	"SRR7646904"
+	"SRR7646964"
 )
-declare samples_to_clean=(
+
+declare -a samples_to_clean=(
 	"SRR7646890"
+	"SRR7646895"
 	"SRR7646896"
 	"SRR7646898"
 	"SRR7646902"
+	"SRR7646904"
 	"SRR7646906"
 	"SRR7646907"
+	"SRR7646915"
+	"SRR7646917"
 	"SRR7646919"
+	"SRR7646926"
+	"SRR7646927"
 	"SRR7646928"
 	"SRR7646929"
+	"SRR7646930"
+	"SRR7646931"
 	"SRR7646932"
 	"SRR7646933"
 	"SRR7646934"
 	"SRR7646935"
 	"SRR7646936"
-	"SRR7646937"
+	# "SRR7646937"
+	"SRR7646961"
 	"SRR7646966"
 	"SRR7646967"
 )
 
-for curr_spl in ${samples}
+
+for curr_spl in "${samples[@]}"
 do
 	echo "Download ${curr_spl} in ${out_dir}"
 	download ${curr_spl} ${out_dir} && rename ${curr_spl} ${out_dir}
-	for curr_clean in ${samples_to_clean} ; do
-		if ${curr_spl} == ${curr_clean} ; then
-			cut ${curr_spl} ${out_dir}
+	for curr_clean in "${samples_to_clean[@]}" ; do
+		if [ ${curr_spl} == ${curr_clean} ] ; then
+			echo "    > Cut ${curr_spl}"
+			cutseq ${curr_spl} ${out_dir}
 		fi
 	done
 done
+
+conda deactivate
 
 echo "End of job"
--- assessment/bin/dl_synthetic_Tembe_2014.sh
+++ assessment/bin/dl_synthetic_Tembe_2014.sh
@@ -18,7 +18,7 @@ download () {
 
 # Rename fastq
 rename () {
-	mv $1_1.fastq.gz $2_R1.fastq.gz && mv $1_2.fastq.gz $2_R2.fastq.gz
+	mv ${1}_1.fastq.gz ${2}/${1}_R1.fastq.gz && mv ${1}_2.fastq.gz ${2}/${1}_R2.fastq.gz
 	if [ $? -ne 0 ]; then
 		>&2 echo "Error on $1"
 		exit 1
@@ -40,32 +40,33 @@ fi
 
 # Download
 declare samples=(
-	#~ "SRR1659960"  # Problem in file
-	#~ "SRR1659966"  # Problem in file
+	"SRR1544075"
+	"SRR1548811"
 	"SRR1659951"
 	"SRR1659952"
 	"SRR1659953"
 	"SRR1659954"
-	"SRR1659959"
 	"SRR1659955"
 	"SRR1659956"
 	"SRR1659957"
 	"SRR1659958"
-	"SRR1548811"
+	"SRR1659959"
+	"SRR1659960"  # Problem in file: quality always ? in all bases in all reads
 	"SRR1659961"
 	"SRR1659962"
 	"SRR1659963"
-	"SRR1659968"
 	"SRR1659964"
 	"SRR1659965"
+	"SRR1659966"
 	"SRR1659967"
-	"SRR1544075"
+	"SRR1659968"
 )
 
-for curr_spl in ${samples}
+
+for curr_spl in "${samples[@]}"
 do
 	echo "Download ${curr_spl} in ${out_dir}"
-	download ${curr_spl} ${out_dir} && rename ${curr_spl}
+	download ${curr_spl} ${out_dir} && rename ${curr_spl} ${out_dir}
 done
 
 echo "End of job"
